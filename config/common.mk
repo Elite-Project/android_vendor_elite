bc97c145de9af4a7f49d772531c66f72b380a048
TheStrix <parthbhatia98@gmail.com>
2018-09-21 22:31:13 +0530

    ELite:prebuilt:Import backuptool from Lineage
    Change-Id: I03d7bf70e41cd00e3abf83f0d3729f72aac6927e
    Signed-off-by: Chaitanaya-Mittal <chaitanayamittal98@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -119,6 +119,19 @@ PRODUCT_PACKAGES += \
     ntfsfix \
     ntfs-3g
 
+# Include support for GApps backup
+PRODUCT_COPY_FILES += \
+    vendor/elite/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/elite/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/elite/prebuilt/bin/50-backuptool.sh:system/addon.d/50-backuptool.sh
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/elite/prebuilt/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/elite/prebuilt/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/elite/prebuilt/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
 # Include vendor overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/elite/overlay/common
 
