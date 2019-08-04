adithya2306/platform_vendor_fred

vendor: sepolicy: move sepolicy to system.
This is needed for QSSIs to be able to use our vendor sepolicies.

This won´t cause any issues to device specific builds.

Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
Change-Id: Ifb4057334110d1c7389f728cbaf573a5538a98d2

sepolicy/sepolicy.mk
@@ -1,2 +1,2 @@
 # This policy configuration will be used by all products that inherit from vendor
-BOARD_SEPOLICY_DIRS += vendor/fred/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/fred/sepolicy

Elite-Project/android_vendor_elite

sepolicy/sepolicy.mk
@@ -1,2 +1,2 @@
 # This policy configuration will be used by all products that inherit from vendor
-BOARD_SEPOLICY_DIRS += vendor/elite/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/elite/sepolicy