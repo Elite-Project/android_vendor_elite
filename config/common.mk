adithya2306/platform_vendor_fred

Initial sepolicy
Change-Id: I0707ae85da0d153a8c383b9eaa08ef9c4057f8fd

config/common.mk
@@ -70,3 +70,6 @@ else
 # include definitions for SDCLANG
 include vendor/fred/sdclang/sdclang.mk
 endif
+
+# Include vendor SEPolicy changes
+include vendor/fred/sepolicy/sepolicy.mk

sepolicy/sepolicy.mk
@@ -1,0 +1,2 @@
+# This policy configuration will be used by all products that inherit from vendor
+BOARD_SEPOLICY_DIRS += vendor/fred/sepolicy

Elite-Project/android_vendor_elite

config/common.mk
@@ -52,3 +52,6 @@ else
 # include definitions for SDCLANG
 include vendor/elite/sdclang/sdclang.mk
 endif
+
+# Include vendor SEPolicy changes
+include vendor/elite/sepolicy/sepolicy.mk

sepolicy/sepolicy.mk
@@ -1,0 +1,2 @@
+# This policy configuration will be used by all products that inherit from vendor
+BOARD_SEPOLICY_DIRS += vendor/elite/sepolicy