adithya2306/platform_vendor_fred

move sdclang config files to vendor
* Do not limit the usage of sdclang to devices which call
  the qcom/common repo

Change-Id: I667e6270058d02fdd894b8242a6375b1a13637dd

config/common.mk
@@ -58,3 +58,15 @@ PRODUCT_COPY_FILES += \
     vendor/fred/prebuilt/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     vendor/fred/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     vendor/fred/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
+
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+# include definitions for SDCLANG
+include vendor/fred/sdclang/sdclang.mk
+endif

sdclang/sdclang.mk
@@ -1,0 +1,15 @@
+# Copyright (C) 2016-2017 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SDCLANG_LTO_DEFS := vendor/fred/sdclang/sdllvm-lto-defs.mk

vendorsetup.sh
@@ -1,3 +1,8 @@
 for device in $(cat $(gettop)/vendor/fred/fred.devices | sed -e 's/#.*$//' | awk '{printf "fred_%s-userdebug\n", $1}'); do
     add_lunch_combo $device;
 done;
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=vendor/fred/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/fred/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false

Elite-Project/android_vendor_elite

config/common.mk
@@ -40,3 +40,15 @@ PRODUCT_COPY_FILES += \
     vendor/elite/prebuilt/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     vendor/elite/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     vendor/elite/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
+
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+# include definitions for SDCLANG
+include vendor/elite/sdclang/sdclang.mk
+endif

sdclang/sdclang.mk
@@ -1,0 +1,15 @@
+# Copyright (C) 2016-2017 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SDCLANG_LTO_DEFS := vendor/elite/sdclang/sdllvm-lto-defs.mk

vendorsetup.sh
@@ -1,3 +1,8 @@
 for device in $(cat $(gettop)/vendor/elite/elite.devices | sed -e 's/#.*$//' | awk '{printf "elite_%s-userdebug\n", $1}'); do
     add_lunch_combo $device;
 done;
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=vendor/elite/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/elite/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false